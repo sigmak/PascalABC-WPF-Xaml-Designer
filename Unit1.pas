b2f066903374d28cb1ec814d72b895520f145597
sigmak <dwfree74@gmail.com>
2026-06-18 02:01:53 +0900

    Ver-1-0-3
    help-about 내용 수정
    toolbox 컨트롤 추가

--- Unit1.pas
+++ Unit1.pas
@@ -17,19 +17,11 @@ type
     fSurface : ICSharpCode.WpfDesign.Designer.DesignSurface;
     fPropView: ICSharpCode.WpfDesign.Designer.PropertyGrid.PropertyGridView;
 
-    // WinForms 레이아웃
-    panel    : System.Windows.Forms.TableLayoutPanel;
-    menuStrip: System.Windows.Forms.MenuStrip;
-
-    // ElementHost (WPF→WinForms 브릿지)
-    hostDesign: System.Windows.Forms.Integration.ElementHost;
-    hostLeft  : System.Windows.Forms.Integration.ElementHost;
-    hostRight : System.Windows.Forms.Integration.ElementHost;
-
-    // 왼쪽 Toolbox (WPF StackPanel)
-    fToolboxPanel: System.Windows.Controls.StackPanel;
-
-    // XAML 텍스트 에디터 (하단)
+    menuStrip     : System.Windows.Forms.MenuStrip;
+    hostDesign    : System.Windows.Forms.Integration.ElementHost;
+    hostLeft      : System.Windows.Forms.Integration.ElementHost;
+    hostRight     : System.Windows.Forms.Integration.ElementHost;
+    fToolboxPanel : System.Windows.Controls.StackPanel;
     splitContainer: System.Windows.Forms.SplitContainer;
     txtXaml       : System.Windows.Forms.RichTextBox;
     btnApply      : System.Windows.Forms.Button;
@@ -37,11 +29,12 @@ type
     procedure BuildMenu;
     procedure BuildToolbox;
     procedure BuildLayout;
-    procedure ConnectPropertyGrid;
+    procedure ConnectEvents;
     procedure AddToolboxButton(name: string; typeName: string);  // ← 클래스 메서드로
     procedure OnToolboxClick(sender: System.Object; e: System.Windows.RoutedEventArgs); // ← 별도 핸들러
     // 선언부에서 시그니처 변경
     procedure OnSelectionChanged(sender: System.Object; e: ICSharpCode.WpfDesign.DesignItemCollectionEventArgs);
+    procedure OnUndoStackChanged(sender: System.Object; e: System.EventArgs);
     procedure OnSave(sender: System.Object; e: System.EventArgs);
     procedure OnOpen(sender: System.Object; e: System.EventArgs);
     procedure OnApplyXaml(sender: System.Object; e: System.EventArgs);  // RoutedEventArgs → EventArgs    
@@ -50,7 +43,8 @@ type
     procedure OnAbout(sender: System.Object; e: System.EventArgs);
     
     procedure LoadXaml(xaml: string);
-    function  FormatXml(xaml: string): string;
+    procedure SyncXamlEditor;
+    function  SaveDesignerToString: string;
   public
     constructor Create;
   end;
@@ -59,7 +53,7 @@ type
 constructor Form1.Create;
 begin
   inherited Create;
-  Self.Text   := 'PascalABC-WPF-Xaml-Designer Ver 1.0.2';
+  Self.Text   := 'PascalABC-WPF-Xaml-Designer Ver 1.0.3';
   Self.Width  := 1500;
   Self.Height := 900;
 
@@ -68,41 +62,50 @@ begin
   BuildMenu;
   BuildToolbox;
   BuildLayout;
+
   LoadXaml(
     '<Grid xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"' +
     '      xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"' +
-    '      Background="White" Width="400" Height="300">' +
-    '  <Button Width="100" Height="30" Content="Hello" />' +
+    '      Background="White" Width="600" Height="400">' +
+    '  <Button Width="100" Height="30" Content="Hello"' +
+    '          HorizontalAlignment="Left" VerticalAlignment="Top"' +
+    '          Margin="20,20,0,0"/>' +
     '</Grid>'
   );
-  ConnectPropertyGrid;
 end;
 
 // ─────────────────────────────────────────────
-// XML 인덴트 포맷 헬퍼
-function Form1.FormatXml(xaml: string): string;
+// 디자이너 내용을 인덴트된 XAML 문자열로 반환
+function Form1.SaveDesignerToString: string;
 var
-  sw : System.IO.StringWriter;
-  xw : System.Xml.XmlWriter;
-  //xtw: System.Xml.XmlTextWriter;
-  doc: System.Xml.XmlDocument;
+  sw        : System.IO.StringWriter;
+  xwSettings: System.Xml.XmlWriterSettings;
+  xw        : System.Xml.XmlWriter;
 begin
-  try
-    doc := new System.Xml.XmlDocument();
-    doc.LoadXml(xaml);
-    sw  := new System.IO.StringWriter();
-    //xtw := new System.Xml.XmlTextWriter(sw);
-    //xtw.Formatting := System.Xml.Formatting.Indented;
-    //xtw.Indentation := 2;
-    //doc.WriteTo(xtw);
-    xw  := new System.Xml.XmlTextWriter(sw);  // XmlTextWriter로 생성
-  (xw as System.Xml.XmlTextWriter).Formatting := System.Xml.Formatting.Indented;   
-    doc.WriteTo(xw);
-    //xtw.Flush();
-    Result := sw.ToString();
-  except
-    Result := xaml;  // 파싱 실패 시 원본 반환
+  if fSurface.DesignContext = nil then
+  begin
+    Result := '';
+    exit;
   end;
+  sw            := new System.IO.StringWriter();
+  xwSettings    := new System.Xml.XmlWriterSettings();
+  xwSettings.Indent      := true;
+  xwSettings.IndentChars := '  ';
+  xw := System.Xml.XmlWriter.Create(sw, xwSettings);
+  fSurface.SaveDesigner(xw);
+  xw.Flush();
+  Result := sw.ToString();
+end;
+
+// ─────────────────────────────────────────────
+// 하단 XAML 에디터를 디자이너 현재 상태로 동기화
+procedure Form1.SyncXamlEditor;
+var
+  s: string;
+begin
+  s := SaveDesignerToString();
+  if s <> '' then
+    txtXaml.Text := s;
 end;
 
 // ─────────────────────────────────────────────
@@ -166,15 +169,19 @@ begin
 
   fToolboxPanel.Children.Add(new System.Windows.Controls.Separator());
 
-  // 일반 컨트롤
-  AddToolboxButton('Button',    'System.Windows.Controls.Button');
-  AddToolboxButton('TextBox',   'System.Windows.Controls.TextBox');
-  AddToolboxButton('Label',     'System.Windows.Controls.Label');
-  AddToolboxButton('CheckBox',  'System.Windows.Controls.CheckBox');
-  AddToolboxButton('ComboBox',  'System.Windows.Controls.ComboBox');
-  AddToolboxButton('ListBox',   'System.Windows.Controls.ListBox');
-  AddToolboxButton('Image',     'System.Windows.Controls.Image');
-  AddToolboxButton('TextBlock', 'System.Windows.Controls.TextBlock');
+  // 컨트롤
+  AddToolboxButton('Button',      'System.Windows.Controls.Button');
+  AddToolboxButton('TextBox',     'System.Windows.Controls.TextBox');
+  AddToolboxButton('Label',       'System.Windows.Controls.Label');
+  AddToolboxButton('CheckBox',    'System.Windows.Controls.CheckBox');
+  AddToolboxButton('ComboBox',    'System.Windows.Controls.ComboBox');
+  AddToolboxButton('ListBox',     'System.Windows.Controls.ListBox');
+  AddToolboxButton('Image',       'System.Windows.Controls.Image');
+  AddToolboxButton('TextBlock',   'System.Windows.Controls.TextBlock');
+  AddToolboxButton('Slider',      'System.Windows.Controls.Slider');
+  AddToolboxButton('ProgressBar', 'System.Windows.Controls.ProgressBar');
+  AddToolboxButton('RadioButton', 'System.Windows.Controls.RadioButton');
+  AddToolboxButton('Border',      'System.Windows.Controls.Border');
 
   scroll         := new System.Windows.Controls.ScrollViewer();
   scroll.Content := fToolboxPanel;
@@ -202,21 +209,23 @@ begin
 end;
 
 // ─────────────────────────────────────────────
-// 툴박스 버튼 클릭 - AppDomain 전체에서 타입 검색
+// 툴박스 클릭 → 루트에 컨트롤 추가
 procedure Form1.OnToolboxClick(sender: System.Object; e: System.Windows.RoutedEventArgs);
 var
-  tname   : string;
-  t       : System.Type;
-  inst    : System.Object;
-  newItem : ICSharpCode.WpfDesign.DesignItem;
-  rootItem: ICSharpCode.WpfDesign.DesignItem;
-  list    : System.Collections.Generic.List<ICSharpCode.WpfDesign.DesignItem>;
+  tname    : string;
+  t        : System.Type;
+  inst     : System.Object;
+  newItem  : ICSharpCode.WpfDesign.DesignItem;
+  rootItem : ICSharpCode.WpfDesign.DesignItem;
+  //grp      : ICSharpCode.WpfDesign.DesignItemChangeGroup;
+  list     : System.Collections.Generic.List<ICSharpCode.WpfDesign.DesignItem>;
+  childProp: ICSharpCode.WpfDesign.DesignItemProperty;
 begin
   if fSurface.DesignContext = nil then exit;
 
   tname := (sender as System.Windows.Controls.Button).Tag.ToString();
 
-  // AppDomain 전체 어셈블리에서 타입 검색
+  // AppDomain 전체에서 타입 검색
   t := nil;
   var asms := System.AppDomain.CurrentDomain.GetAssemblies();
   var i := 0;
@@ -228,38 +237,51 @@ begin
 
   if t = nil then
   begin
-    System.Windows.Forms.MessageBox.Show(
-      '타입을 찾을 수 없습니다: ' + tname, '오류',
-      System.Windows.Forms.MessageBoxButtons.OK,
-      System.Windows.Forms.MessageBoxIcon.Error);
+    System.Windows.Forms.MessageBox.Show('타입을 찾을 수 없습니다: ' + tname);
     exit;
   end;
 
   try
     inst := System.Activator.CreateInstance(t);
+    if inst = nil then exit;
+
+    var services := fSurface.DesignContext.Services;
+    rootItem  := fSurface.DesignContext.RootItem;
+
+    // ChangeGroup으로 Undo 지원하며 추가
+    var grp     := rootItem.OpenGroup('Add ' + tname);
+    newItem := services.Component.RegisterComponentForDesigner(inst);
+
+    // 루트 ContentProperty(Children 컬렉션)에 추가
+    childProp := rootItem.ContentProperty;
+    if childProp <> nil then
+    begin
+      if childProp.IsCollection then
+        childProp.CollectionElements.Add(newItem)
+      else
+        childProp.SetValue(newItem);
+    end;
+
+    grp.Commit();
+
+    // 추가된 컨트롤 선택
+    list := new System.Collections.Generic.List<ICSharpCode.WpfDesign.DesignItem>();
+    list.Add(newItem);
+    services.Selection.SetSelectedComponents(
+      list as System.Collections.Generic.ICollection<ICSharpCode.WpfDesign.DesignItem>
+    );
+
+    // XAML 에디터 즉시 동기화
+    SyncXamlEditor();
+
   except
     on ex: System.Exception do
-    begin
-      System.Windows.Forms.MessageBox.Show('인스턴스 생성 실패: ' + ex.Message);
-      exit;
-    end;
+      System.Windows.Forms.MessageBox.Show('컨트롤 추가 실패: ' + ex.Message);
   end;
-
-  if inst = nil then exit;
-
-  var services := fSurface.DesignContext.Services;
-  rootItem := fSurface.DesignContext.RootItem;
-  newItem  := services.Component.RegisterComponentForDesigner(rootItem, inst);
-
-  list := new System.Collections.Generic.List<ICSharpCode.WpfDesign.DesignItem>();
-  list.Add(newItem);
-  services.Selection.SetSelectedComponents(
-    list as System.Collections.Generic.ICollection<ICSharpCode.WpfDesign.DesignItem>
-  );
 end;
 
 // ─────────────────────────────────────────────
-// A. 3분할 레이아웃 + C. XAML 에디터
+// 레이아웃
 procedure Form1.BuildLayout;
 var
   topPanel: System.Windows.Forms.TableLayoutPanel;
@@ -322,42 +344,47 @@ begin
 end;
 
 // ─────────────────────────────────────────────
-// XAML 로드 - 인덴트 포맷 적용
+// XAML 로드 + 이벤트 연결
 procedure Form1.LoadXaml(xaml: string);
 var
-  strReader : System.IO.StringReader;
-  xmlReader : System.Xml.XmlReader;
-  settings  : ICSharpCode.WpfDesign.Designer.Xaml.XamlLoadSettings;
-  sw        : System.IO.StringWriter;
-  xwSettings: System.Xml.XmlWriterSettings;
-  xw        : System.Xml.XmlWriter;
+  strReader: System.IO.StringReader;
+  xmlReader: System.Xml.XmlReader;
+  settings : ICSharpCode.WpfDesign.Designer.Xaml.XamlLoadSettings;
 begin
   fSurface  := new ICSharpCode.WpfDesign.Designer.DesignSurface();
   settings  := new ICSharpCode.WpfDesign.Designer.Xaml.XamlLoadSettings();
   strReader := new System.IO.StringReader(xaml);
   xmlReader := new System.Xml.XmlTextReader(strReader);
   fSurface.LoadDesigner(xmlReader, settings);
-
   hostDesign.Child := fSurface;
 
-  sw         := new System.IO.StringWriter();
-  xwSettings := new System.Xml.XmlWriterSettings();
-  xwSettings.Indent      := true;
-  xwSettings.IndentChars := '  ';
-  xw := System.Xml.XmlWriter.Create(sw, xwSettings);
-  fSurface.SaveDesigner(xw);
-  xw.Flush();
-  txtXaml.Text := sw.ToString();
+  ConnectEvents();
+  SyncXamlEditor();
 end;
 
 // ─────────────────────────────────────────────
-// A. PropertyGrid 와 선택 이벤트 연결
-procedure Form1.ConnectPropertyGrid;
+// 이벤트 연결
+procedure Form1.ConnectEvents;
+var
+  undoSvc: ICSharpCode.WpfDesign.Designer.Services.UndoService;
 begin
   if fSurface.DesignContext = nil then exit;
+
+  // 선택 변경 → PropertyGrid 갱신
   fSurface.DesignContext.Services.Selection.SelectionChanged += OnSelectionChanged;
+
+  // UndoService 변경 → XAML 에디터 자동 동기화
+  // 드래그/크기변경/속성변경 등 모든 디자인 변경을 감지함
+  undoSvc := fSurface.DesignContext.Services.GetService(
+    typeof(ICSharpCode.WpfDesign.Designer.Services.UndoService)
+  ) as ICSharpCode.WpfDesign.Designer.Services.UndoService;
+
+  if undoSvc <> nil then
+    undoSvc.UndoStackChanged += OnUndoStackChanged;
 end;
 
+// ─────────────────────────────────────────────
+// 선택 변경 → PropertyGrid 업데이트
 procedure Form1.OnSelectionChanged(sender: System.Object;
   e: ICSharpCode.WpfDesign.DesignItemCollectionEventArgs);
 begin
@@ -367,13 +394,18 @@ begin
 end;
 
 // ─────────────────────────────────────────────
-// B. 저장
+// 디자인 변경(드래그/크기/속성) → XAML 자동 동기화
+procedure Form1.OnUndoStackChanged(sender: System.Object; e: System.EventArgs);
+begin
+  SyncXamlEditor();
+end;
+
+// ─────────────────────────────────────────────
+// 저장
 procedure Form1.OnSave(sender: System.Object; e: System.EventArgs);
 var
-  dlg       : System.Windows.Forms.SaveFileDialog;
-  sw        : System.IO.StringWriter;
-  xwSettings: System.Xml.XmlWriterSettings;
-  xw        : System.Xml.XmlWriter;
+  dlg : System.Windows.Forms.SaveFileDialog;
+  xaml: string;
 begin
   if fSurface.DesignContext = nil then exit;
   dlg          := new System.Windows.Forms.SaveFileDialog();
@@ -381,77 +413,70 @@ begin
   dlg.FileName := 'design.xaml';
   if dlg.ShowDialog() = System.Windows.Forms.DialogResult.OK then
   begin
-    sw         := new System.IO.StringWriter();
-    xwSettings := new System.Xml.XmlWriterSettings();
-    xwSettings.Indent      := true;
-    xwSettings.IndentChars := '  ';
-    xw := System.Xml.XmlWriter.Create(sw, xwSettings);
-    fSurface.SaveDesigner(xw);
-    xw.Flush();
-    System.IO.File.WriteAllText(dlg.FileName, sw.ToString());
-    System.Windows.Forms.MessageBox.Show('저장 완료: ' + dlg.FileName);
+    xaml := SaveDesignerToString();
+    if xaml <> '' then
+    begin
+      System.IO.File.WriteAllText(dlg.FileName, xaml);
+      System.Windows.Forms.MessageBox.Show('저장 완료: ' + dlg.FileName);
+    end;
   end;
 end;
 
 // ─────────────────────────────────────────────
-// B. 열기 - Window/UserControl 루트를 Grid로 변환
+// 열기 - Window/UserControl 루트를 Grid로 변환
 procedure Form1.OnOpen(sender: System.Object; e: System.EventArgs);
 var
-  dlg : System.Windows.Forms.OpenFileDialog;
-  xaml: string;
-  doc : System.Xml.XmlDocument;
-  root: System.Xml.XmlElement;
+  dlg  : System.Windows.Forms.OpenFileDialog;
+  xaml : string;
+  doc  : System.Xml.XmlDocument;
+  root : System.Xml.XmlElement;
   inner: string;
 begin
-  dlg := new System.Windows.Forms.OpenFileDialog();
+  dlg        := new System.Windows.Forms.OpenFileDialog();
   dlg.Filter := 'XAML 파일|*.xaml|모든 파일|*.*';
-  if dlg.ShowDialog() = System.Windows.Forms.DialogResult.OK then
+  if dlg.ShowDialog() <> System.Windows.Forms.DialogResult.OK then exit;
+
+  xaml := System.IO.File.ReadAllText(dlg.FileName);
+
+  // Window / UserControl 루트 → Grid로 변환
+  if xaml.Contains('<Window ') or xaml.Contains('<UserControl ') then
   begin
-    xaml := System.IO.File.ReadAllText(dlg.FileName);
-
-    // Window 또는 UserControl 루트인 경우 Grid로 래핑
-    if xaml.Contains('<Window ') or xaml.Contains('<UserControl ') then
-    begin
-      try
-        doc := new System.Xml.XmlDocument();
-        doc.LoadXml(xaml);
-        root  := doc.DocumentElement;
-        inner := root.InnerXml;
-        xaml  :=
-          '<Grid xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"' +
-          '      xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">' +
-          inner +
-          '</Grid>';
-        System.Windows.Forms.MessageBox.Show(
-          'Window/UserControl 루트를 Grid로 변환하여 로드합니다.',
-          '알림', System.Windows.Forms.MessageBoxButtons.OK,
-          System.Windows.Forms.MessageBoxIcon.Information);
-      except
-        on ex: System.Exception do
-        begin
-          System.Windows.Forms.MessageBox.Show('XAML 변환 오류: ' + ex.Message);
-          exit;
-        end;
-      end;
-    end;
-
     try
-      LoadXaml(xaml);
-      ConnectPropertyGrid;
+      doc   := new System.Xml.XmlDocument();
+      doc.LoadXml(xaml);
+      root  := doc.DocumentElement;
+      inner := root.InnerXml;
+      xaml  :=
+        '<Grid xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"' +
+        '      xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">' +
+        inner + '</Grid>';
+      System.Windows.Forms.MessageBox.Show(
+        'Window/UserControl 루트를 Grid로 변환하여 로드합니다.',
+        '알림', System.Windows.Forms.MessageBoxButtons.OK,
+        System.Windows.Forms.MessageBoxIcon.Information);
     except
       on ex: System.Exception do
-        System.Windows.Forms.MessageBox.Show('XAML 로드 오류: ' + ex.Message);
+      begin
+        System.Windows.Forms.MessageBox.Show('XAML 변환 오류: ' + ex.Message);
+        exit;
+      end;
     end;
+  end;
+
+  try
+    LoadXaml(xaml);
+  except
+    on ex: System.Exception do
+      System.Windows.Forms.MessageBox.Show('XAML 로드 오류: ' + ex.Message);
   end;
 end;
 
 // ─────────────────────────────────────────────
-// C. XAML 텍스트 → 디자이너 적용
+// 하단 XAML → 디자이너 적용
 procedure Form1.OnApplyXaml(sender: System.Object; e: System.EventArgs);
 begin
   try
     LoadXaml(txtXaml.Text);
-    ConnectPropertyGrid;
   except
     on ex: System.Exception do
       System.Windows.Forms.MessageBox.Show('XAML 오류: ' + ex.Message);
@@ -459,29 +484,17 @@ begin
 end;
 
 // ─────────────────────────────────────────────
-// C. 디자이너 → XAML 텍스트 동기화 (메뉴)
+// 메뉴: 수동 동기화
 procedure Form1.OnSyncXamlMenu(sender: System.Object; e: System.EventArgs);
-var
-  sw        : System.IO.StringWriter;
-  xwSettings: System.Xml.XmlWriterSettings;
-  xw        : System.Xml.XmlWriter;
 begin
-  if fSurface.DesignContext = nil then exit;
-  sw         := new System.IO.StringWriter();
-  xwSettings := new System.Xml.XmlWriterSettings();
-  xwSettings.Indent      := true;
-  xwSettings.IndentChars := '  ';
-  xw := System.Xml.XmlWriter.Create(sw, xwSettings);
-  fSurface.SaveDesigner(xw);
-  xw.Flush();
-  txtXaml.Text := sw.ToString();
+  SyncXamlEditor();
 end;
 // Help > About
 procedure Form1.OnAbout(sender: System.Object; e: System.EventArgs);
 begin
   System.Windows.Forms.MessageBox.Show(
     'PascalABC-WPF-Xaml-Designer' + System.Environment.NewLine +
-    'Ver 1.0.2' + System.Environment.NewLine + System.Environment.NewLine +
+    'Ver 1.0.3' + System.Environment.NewLine + System.Environment.NewLine +
     'ICSharpCode.WpfDesign.Designer' + System.Environment.NewLine +
     'ICSharpCode.WpfDesign' + System.Environment.NewLine +
     'ICSharpCode.WpfDesign.XamlDom' + System.Environment.NewLine +
